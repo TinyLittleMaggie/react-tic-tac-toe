46e0821a0a3f7c0442823b309423cb5a2686d51c
Maggie Gong <meijic@outlook.com>
2020-02-28 19:25:36 +0800

    calculate winner

--- src/index.js
+++ src/index.js
@@ -27,8 +27,33 @@ class Board extends React.Component {
     }
   }
 
+  detectWinner() {
+    const squares = this.state.squares;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.detectWinner() || squares[i]) {
+      // ignore click if someone has won the game or the square is filled
+      return;
+    }
     squares[i] = this.state.player;
     this.setState({ squares: squares });
     this.togglePlayer();
@@ -44,8 +69,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + this.state.player;
-
+    const winner = this.detectWinner();
+    const status = winner ? winner + ' wins!' : 'Next player: ' + this.state.player;
     return (
       <div>
         <div className="status">{status}</div>
