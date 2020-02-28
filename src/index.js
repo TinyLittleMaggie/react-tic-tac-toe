b6a252e7776a2a9457c3ce1f235085b8be8d4d6d
Maggie Gong <meijic@outlook.com>
2020-02-28 22:56:15 +0800

    add data and display card according to selection

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import './index.css';
 function Card(props) {
   return (
     <div className="card">
-      {props.value}
+      <h2>{props.data.productFamily.name}</h2>
+      <p>{props.data.cpType.desc}</p>
+      <p>Finished: {props.data.finished.text}</p>
+      <p>Review date: {props.data.review_month + " " + props.data.review_day}</p>
     </div>
   );
 }
@@ -19,7 +22,6 @@ class Dropdown extends React.Component {
           <option value="grapefruit">Grapefruit</option>
           <option value="lime">Lime</option>
           <option value="coconut">Coconut</option>
-          <option value="mango">Mango</option>
         </select>
       </div>
     );
@@ -29,23 +31,60 @@ class Dropdown extends React.Component {
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'coconut'};
+    this.state = {
+      selection: 'coconut',
+      data: [
+        {
+          cpType: {desc: "I am a description!", fontColor: "purple", pillBg: "white", borderColor: "purple"},
+          finished: {text: "Needs Review", bgColor: "red"},
+          id: 36,
+          productFamily: {id: 86, name: "coconut"},
+          review_day: "28",
+          review_month: "Feb"
+        },
+        {
+          cpType: {desc: "I am a different description!", fontColor: "purple", pillBg: "white", borderColor: "purple"},
+          finished: {text: "Finished", bgColor: "red"},
+          id: 37,
+          productFamily: {id: 86, name: "lime"},
+          review_day: "2",
+          review_month: "Mar"
+        },
+        {
+          cpType: {desc: "I am the best grapefruit!", fontColor: "purple", pillBg: "white", borderColor: "purple"},
+          finished: {text: "Needs Review", bgColor: "red"},
+          id: 38,
+          productFamily: {id: 86, name: "grapefruit"},
+          review_day: "30",
+          review_month: "Jan"
+        }
+      ]
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({selection: event.target.value});
+  }
+
+  chooseData(selection) {
+    const data = this.state.data.slice();
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].productFamily.name === selection) {
+        return data[i];
+      }
+    }
   }
 
   render(event) {
     return (
       <div className="form">
         <Dropdown
-          value={this.state.value}
+          value={this.state.selection}
           onClick={(event) => this.handleChange(event)}
         />
-        <Card value={this.state.value}/>
+        <Card data={this.chooseData(this.state.selection)}/>
       </div>
     );
   }
